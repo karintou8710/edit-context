5eade71409bf246088cd6eab938afe7a738804d5
karintou8710 <karintou8710@gmail.com>
2025-12-23 01:04:06 +0900

    chore: add biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
 
 function App() {
-  const ref = useRef<HTMLDivElement>(null)
+	const ref = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (!ref.current) return
+	useEffect(() => {
+		if (!ref.current) return;
 
-    const el = ref.current
-    const editContext = new EditContext({
-      text: el.textContent || '',
-    })
+		const el = ref.current;
+		const editContext = new EditContext({
+			text: el.textContent || "",
+		});
 
-    el.editContext = editContext
+		el.editContext = editContext;
 
-    editContext.addEventListener('textupdate', event => {
-      const sel = window.getSelection();
-      el.textContent = editContext.text;
-      sel?.collapse(el.firstChild, event.selectionStart);
-    });
+		editContext.addEventListener("textupdate", (event) => {
+			const sel = window.getSelection();
+			el.textContent = editContext.text;
+			sel?.collapse(el.firstChild, event.selectionStart);
+		});
 
-    document.addEventListener('selectionchange', () => {
-      const selection = window.getSelection();
+		document.addEventListener("selectionchange", () => {
+			const selection = window.getSelection();
+			if (!selection) return;
 
-      editContext.updateSelection(selection!.anchorOffset, selection!.focusOffset);
-    });
-  }, [])
+			editContext.updateSelection(
+				selection.anchorOffset,
+				selection.focusOffset,
+			);
+		});
+	}, []);
 
-  return <div>
-    <div ref={ref}>Hello World</div>
-  </div>
+	return (
+		<div>
+			<div ref={ref}>Hello World</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
